b2e5cfeed2c1a5c620d1b1bb0812faf9c59c0b28
tyronepost <tyrone.post.a@gmail.com>
2016-07-05 19:35:22 -0400

    added Recipe scaffold

---
+++ app/views/recipes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @recipe, :id, :name, :ingredients, :directions, :created_at, :updated_at
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Recipe.create(name: 'tacos', ingredients: 'salt, pepper, ground beef', directions: 'cook meat until brown')
