b9999e892fcd78d09e2d41a35f94a2176c98b44b
tyronepost <tyrone.post.a@gmail.com>
2016-07-05 20:02:14 -0400

    adding users scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705231240) do
+ActiveRecord::Schema.define(version: 20160705234740) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20160705231240) do
     t.text     "directions"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
